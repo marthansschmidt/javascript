f32e35bd176c73da85369b265848bc3285660dfe
Mac <mac@Macs-MacBook-Air.local>
2025-10-15 15:10:49 +0300

    essa tund

---
+++ index.js
@@ -0,0 +1,27 @@
+// console.log(`Hello`);
+// console.log(`I love pizza!`);
+
+// window.alert(`This is an alert!`);
+// window.alert(`I like pizza!`);
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like pizza!`
+
+let x;
+x = 100;
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+console.log(`You are ${age} years old`);
+console.log(`The price is $${price}`);
+console.log(`Your gpa is: ${gpa}`);
+// This is a comment 
+
+/* 
+    This   
+    is  
+    a 
+    comment
+/*
