44ab16912a506e35f92107139691ff54791529c0
Mac <mac@Macs-MacBook-Air.local>
2025-10-16 10:57:51 +0300

    tunnitoo

--- index.js
+++ index.js
@@ -4,24 +4,127 @@
 // window.alert(`This is an alert!`);
 // window.alert(`I like pizza!`);
 
-document.getElementById("myH1").textContent = `Hello`;
-document.getElementById("myP").textContent = `I like pizza!`
+// document.getElementById("myH1").textContent = `Hello`;
+// document.getElementById("myP").textContent = `I like pizza!`;
 
-let x;
-x = 100;
+// let x;
+// x = 100;
 
-let age = 25;
-let price = 10.99;
-let gpa = 2.1;
+// let age = 25;
+// let price = 10.99;
+// let gpa = 2.1;
 
-console.log(`You are ${age} years old`);
-console.log(`The price is $${price}`);
-console.log(`Your gpa is: ${gpa}`);
+// let online = false;
+// let forSale = true;
+// let isStudent = true;
+
+// console.log(`Bro is online: ${online}`);
+// console.log(`Is this car for sale $${forSale}`);
+// console.log(`Enrolled: ${isStudent}`);
 // This is a comment 
 
-/* 
-    This   
-    is  
-    a 
-    comment
-/*
+// variable = A container that stores a value. Behaves as if it were the value it contains
+// Muutuja nimi peab olema kasutajale võimalikult selge
+
+// let fullName = "Bro Code";
+// let age = 25
+// let student = false;
+
+// document.getElementById("p1").textContent = fullName;
+// window.getElementById("p2").textContent = fullName;
+
+// console.log(document);
+// console.log(window);
+
+// document.getElementById("p1").textContent = `Your name is ${fullName}`
+// document.getElementById("p2").textContent = `You are`
+
+// let is$Student = false;
+
+// console.log(typeof isStudent);
+
+// kotntrolli, mida maret kirjutas
+
+// let students = 30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2;
+// let extrastudents = students % 17;
+
+// students +=1;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// students %= 2;
+
+// students++;
+// students--;
+
+
+// console.log(students);
+
+// let result = 1 + 6 + 16
+// let result = 12 % 5 + 8 / 2;
+
+// console.log(result);
+
+// 1. sulud
+// 2. ruutjuur, astmes
+// 3. korrutamine, jagamine, jääk
+// 4. liitmine, lahutamine
+
+
+
+// Kasutaja andmete küsimine
+
+// let username;
+
+// username = window.prompt("What's your username");
+
+// console.log(username);
+
+// document.getElementById("mySubmit").onclick = function(){
+//     username = document.getElementById("myText").value;
+//     console.log(username);
+// }
+
+// let age = window.prompt("How old are you?");
+// age= Number(age);
+// age+=1;
+
+// console.log(age, typeof age);
+
+// let x = "pizza";
+// let y = "pizza";
+// let z = "pizza";
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+// let PI = 3.14159;
+// let radius;
+// let circumference;
+
+// // radius = window.prompt(`Enter the radius of a circle`);
+// // radius = Number(radius);
+
+// // circumference = 2 * pi * radius;
+
+// document.getElementById("mySubmit").onclick = function(){
+//     radius = document.getElementById("myText").value;
+//     radius = Number(radius);
+//     circumference = 2 * PI * radius;
+//     document.getElementById("myH3").textContent = circumference;
+// }
+
+
+
